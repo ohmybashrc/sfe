342a122b48d06276897499fe100c0e24c51ad88c
TylerMS887 <115214762+TylerMS887@users.noreply.github.com>
2023-11-02 18:50:27 +0000

    Update sfe.sh

--- sfe.sh
+++ sfe.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# Clear console, save for terminal emulators
+# Save for terminal emulators
 printf '\e[?1049h'
-printf '\e[2J'
 
 get_term_size() {
     # '\e7':           Save the current cursor position.
@@ -11,6 +10,33 @@ get_term_size() {
     # '\e8':           Restore the cursor to its previous position.
     IFS='[;' read -sp $'\e7\e[9999;9999H\e[6n\e8' -d R -rs _ LINES COLUMNS
 }
+get_term_size
+
 # Trap the window resize signal (handle window resize events).
-# See: 'man trap' and 'trap -l'
-trap 'get_term_size' WINCH
+# See: 'help trap' and 'trap -l'
+trap 'get_term_size; draw' WINCH
+
+# Prevent usage of Ctrl+C to terminate
+trap '' INT
+
+# Function to draw UI
+draw() {
+    printf "\e[0;$LINESr\e[2J\e[H\e[1m"
+    export TITLE="SFE 0.1"
+    export FILE="test"
+    printf "\e[7m"
+    printf "  $TITLE"
+    export SPACENUMBER=$(expr $COLUMNS - ${#FILE})
+    export SPACENUMBER=$(expr $SPACENUMBER - ${#TITLE})
+    export SPACENUMBER=$(expr $SPACENUMBER - 4)
+    printf '%*s' $SPACENUMBER
+    printf "$FILE  "
+    printf "\033[0m"
+    printf "\n$SCREEN_TEXT"
+}
+
+# Constantly read and draw
+while true; do
+    draw
+    read -n 1
+done
